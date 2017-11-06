d047186b39695cef4bf788e61c84da33a8d5e237
Le Tat Tung <letattungtb@gmail.com>
2017-11-06 12:45:41 +0700

    refactor actor feature

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -2,23 +2,6 @@ class ActorsController < ApplicationController
   before_action :load_genres, only: [:index, :show]
   before_action :find_actor, :load_messages
 
-  def new
-    @actor = Actor.new
-  end
-
-  def create
-    @actor = Actor.new actor_params
-    respond_to do |format|
-      if @actor.save
-        format.html { redirect_to @actor, notice: "Actor was successfully created." }
-        format.json { render :show, status: :created, location: @actor }
-      else
-        format.html { render :new }
-        format.json { render json: @actor.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   def index
     @actors = Actor.paginate(page: params[:page],
       per_page: Settings.page_actor_size).order(id: :desc)
@@ -30,33 +13,10 @@ class ActorsController < ApplicationController
     end
   end
 
-  def edit; end
-
-  def update
-    respond_to do |format|
-      if @actor.update(actor_params)
-        format.html { redirect_to @actor, notice: "Actor was successfully update." }
-        format.json { render :show, status: :created, location: @actor }
-      else
-        format.html { render :edit }
-        format.json { render json: @actor.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    @actor.destroy
-    respond_to do |format|
-      format.html { redirect_to actors_path, notice: "Actor was successfully destroyed." }
-      format.json { head :no_content }
-    end
+  def edit
   end
 
   private
-
-  def actor_params
-    params.require(:actor).permit :name, :date_of_birth, :avatar
-  end
 
   def load_genres
     @genres = Genre.all
