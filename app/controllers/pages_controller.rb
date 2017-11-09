1604d85f66ec1664d258d01b4c32d54233be0eee
Le Tat Tung <letattungtb@gmail.com>
2017-11-09 14:34:28 +0700

    fix layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,20 +5,10 @@ class PagesController < ApplicationController
   before_action :load_data, only: [:show]
 
   def show
-    if valid_page?
-      render template: "pages/#{params[:page_temp]}"
-    else
-      render file: "public/404.html", status: :not_found
-    end
     @reviews = Review.top_reviews.includes(:comments, :movie)
   end
 
   private
-  def valid_page?
-    File.exist? Pathname
-      .new(Rails.root + "app/views/pages/#{params[:page_temp]}.html.erb")
-  end
-
   def load_reviews
     @reviews = Review.all
   end
