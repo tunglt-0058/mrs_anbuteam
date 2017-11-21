87cdc92d5ffab32af4a95f6b0c65f27be90b1da1
Le Tat Tung <letattungtb@gmail.com>
2017-11-21 15:00:12 +0700

    fix search

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,4 +1,4 @@
-var timer, length_str = 100, length_item = 4;
+var timer, length_str = 27, length_item = 4;
 $(document).on('keyup', '#movies-search', function() {
   clearTimeout(timer);
   timer = setTimeout(function () {
@@ -9,6 +9,31 @@ $(document).on('keyup', '#movies-search', function() {
       dataType: 'json'
     }).done(function(data) {
       $('.results').empty();
+
+      var movies = data.results["movies"];
+      if(movies.length) {
+        $('.results').append('<p><b>映画</b></p><hr>');
+        for (var i = 0; i < movies.length; i++) {
+          $('.results').append('<a href="/movies/' + movies[i].id
+            +'"><li><span class="movies-search-img"><img src="'
+            + movies[i].poster + '"></span><span class="movies-search-str">'
+            + movies[i].name.substring(0, length_str) + '</span></li></a><hr>');
+          if(i == length_item) break;
+        }
+      }
+
+      var actors = data.results["actors"];
+      if(actors.length) {
+        $('.results').append('<p><b>俳優</b></p><hr>');
+        for (var i = 0; i < actors.length; i++) {
+          $('.results').append('<a href="/actors/' + actors[i].id
+            +'"><li><span class="movies-search-img"><img src="'
+            + actors[i].avatar + '"></span><span class="movies-search-str">'
+            + actors[i].name.substring(0, length_str) + '</span></li></a><hr>');
+          if(i == length_item) break;
+        }
+      }
+
       $('.results').append('<a href="/searches?search=' + search);
     });
   }, 100);
